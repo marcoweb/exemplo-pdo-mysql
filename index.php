76c1e3e08e46c49fe414b53de2813930a83d11e7
marcoweb <marcocarvalho.web@gmail.com>
2021-09-19 18:27:11 +0000

    Select Genero

--- index.php
+++ index.php
@@ -1,12 +1,35 @@
+<?php
+require_once './vendor/autoload.php';
+
+use PDO;
+
+$bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+
+$comando = $bd->prepare('SELECT * FROM generos');
+$comando->execute();
+
+$generos = $comando->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
+    <html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Biblioteca</title>
+    </head>
+    <body>
+        <table>
+            <tr>
+                <th>Id</th>
+                <th>Nome</th>
+            </tr>
+            <?php foreach($generos as $g): ?>
+            <tr>
+                <td><?= $g['id'] ?></td>
+                <td><?= $g['nome'] ?></td>
+            </tr>
+            <?php endforeach ?>
+        </table>
+    </body>
 </html>
