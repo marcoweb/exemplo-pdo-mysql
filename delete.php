d1152649caedbf592771ac6ea8bb7a493fc96e58
marcoweb <marcocarvalho.web@gmail.com>
2021-10-15 14:20:02 +0000

    Adicionando Bootstrap

--- delete.php
+++ delete.php
@@ -4,46 +4,38 @@ require_once './vendor/autoload.php';
 use ExemploPDOMySQL\MySQLConnection;
 
 $bd = new MySQLConnection();
-
 $genero = null;
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
     $comando = $bd->prepare('SELECT * FROM generos WHERE id = :id');
     $comando->execute([':id' => $_GET['id']]);
-
     $genero = $comando->fetch(PDO::FETCH_ASSOC);
 } else {
-    $comando = $bd->prepare('UPDATE generos SET nome = :nome WHERE id = :id');
-    $comando->execute([':nome' => $_POST['nome'],':id' => $_POST['id']]);
+    $comando = $bd->prepare('DELETE FROM generos WHERE id = :id');
+    $comando->execute([':id' => $_POST['id']]);
 
     header('Location:/index.php');
 }
 
 ?>
-
 <!DOCTYPE html>
-    <html lang="pt-br">
+<html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <title>Editar Gênero</title>
+        <title>Remover Gênero</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
         <main class="container">
-            <h1>Editar Gênero</h1>
-            <form action="update.php" method="post">
+            <h1>Remover Gênero</h1>
+            <p>Tem certeza que deseja excluir o gênero <?= $genero['nome'] ?>?</p>
+
+            <form action="delete.php" method="post">
                 <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
-                <div class="form-group">
-                    <label for="nome">Nome</label>
-                    <input class="form-control" type="text" name="nome" value="<?= $genero['nome'] ?>" />
-                </div>
-                <br />
                 <a class="btn btn-secondary" href="index.php">Voltar</a>
-                <button class="btn btn-success" type="submit">Salvar</button>
+                <button class="btn btn-danger" type="submit">Excluir</button>
             </form>
         </main>
     </body>
 </html>
-
-
 
